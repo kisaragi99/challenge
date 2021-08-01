1c8681fed753e960702536671ff5d567df78ec81
Kisaragi <budzhalov99@gmail.com>
2021-08-01 22:00:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <>
+    <div>hello react</div>
+  </>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        port: 9000,
+        contentBase: path.join(__dirname, 'public'),
+    },
+};
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['js'] }) ];
+
+module.exports = ({ development }) => ({
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : 'source-map',
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: "babel-loader"
+                }
+              },
+              {
+                test: /\.css$/,
+                use: [
+                  "style-loader",
+                  {
+                    loader: "css-loader",
+                    options: {
+                      modules: true
+                    }
+                  }
+                ]
+              },
+        ]
+    },
+    plugins: [
+        ...esLintPlugin(development),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new CopyPlugin({
+            patterns: [{
+                from: './public',
+                noErrorOnMissing: true,
+            }],
+        }),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ],
+    resolve: {
+        extensions: ['.js','.jsx']
+    },
+    ...devServer(development),
+});
